26f2da7623e981cff15ff01cbd4bfb0ff3413cf9
Misbah Ahmed Araf <arafallen0@gmail.com>
2026-01-02 16:38:07 +0600

    Create Average 3.c

---
+++ Level-1/Average 3.c
@@ -0,0 +1,45 @@
+/*
+
+
+Read four numbers (N1, N2, N3, N4), which one with 1 digit after the decimal point, corresponding to 4 scores obtained by a student. Calculate the average with weights 2, 3, 4 e 1 respectively, for these 4 scores and print the message "Media: " (Average), followed by the calculated result. If the average was 7.0 or more, print the message "Aluno aprovado." (Approved Student). If the average was less than 5.0, print the message: "Aluno reprovado." (Reproved Student). If the average was between 5.0 and 6.9, including these, the program must print the message "Aluno em exame." (In exam student).
+
+In case of exam, read one more score. Print the message "Nota do exame: " (Exam score) followed by the typed score. Recalculate the average (sum the exam score with the previous calculated average and divide by 2) and print the message “Aluno aprovado.” (Approved student) in case of average 5.0 or more) or "Aluno reprovado." (Reproved student) in case of average 4.9 or less. For these 2 cases (approved or reproved after the exam) print the message "Media final: " (Final average) followed by the final average for this student in the last line.
+Input
+
+The input contains four floating point numbers that represent the students' grades.
+Output
+
+Print all the answers with one digit after the decimal point.
+*/
+
+#include <stdio.h>
+int main ()
+{
+    double N_1,N_2, N_3, N_4 ;
+    scanf("%lf %lf %lf %lf", &N_1,&N_2,&N_3,&N_4);
+    double calc =((N_1 *2) +(N_2 *3)+ (N_3 *4)+ (N_4 *1));
+    double score = calc / (10.0);
+    printf("Media: %.1lf\n",score);
+
+    if (calc/10 >=7.0)
+        printf("Aluno aprovado.\n");
+
+    else if (calc/10 >=5.0 && calc/10 <=6.9)
+    {
+        printf("Aluno em exame.\n");
+        double read1;
+        scanf("%lf",&read1);
+        printf("Nota do exame: %.1lf\n",read1);
+        double newscore1= (score + read1)/2;
+        if(newscore1 >= 5.0)
+            printf("Aluno aprovado.\n");
+        else
+            printf("Aluno reprovado.\n");
+        printf("Media final: %.1lf\n",newscore1);
+    }
+
+    else
+        printf("Aluno reprovado.\n");
+    return 0;
+}
+
