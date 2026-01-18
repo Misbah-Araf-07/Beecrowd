a66ba15edb0b35f50d5a167b125c0c5f0dbe01f7
Misbah Ahmed Araf <arafallen0@gmail.com>
2026-01-19 01:22:48 +0600

    Update Sequence IJ 4.c

--- Level-1/Sequence IJ 4.c
+++ Level-1/Sequence IJ 4.c
@@ -16,3 +16,37 @@ Make a program that prints the sequence like the following example.
 
 
 */
+
+
+#include <stdio.h>
+
+int main()
+{   
+    int t, counter=0;
+    double I=0, J=1;
+    double J_og= J;
+
+    for (t=0; t<=10; t++)
+    {
+        I = t * 0.2;
+        J = J_og + I;
+        counter = 0;
+
+        while (counter < 3)
+        {
+            if ( I==0 || I==1 || I==2 )
+                printf("I=%0.lf ", I);
+            else
+                printf("I=%.1lf ", I);
+
+            if ( J==1 || J==2 || J==3 || J==4 || J==5 )
+                printf("J=%0.lf\n", J);
+            else
+                printf("J=%.1lf\n", J);
+
+            J++;
+            counter++;
+        }
+    }
+    return 0;
+}
